e499ed4e8275a3c0eca903ec121650179b92bf51
Paula Cano <paulacanoroman@gmail.com>
2023-12-28 11:15:18 +0100

    feaT: style screen

--- ScreenAddToCart.jsx
+++ ScreenAddToCart.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Carrito from './components/carrito/Carrito.jsx'
 import Productos from './components/productos/Productos.jsx'
 import { ContextAppProvider } from '@/context/CartContextApp'
-import styles from '@/styles/Home.module.css'
+import styles from './styles/Home.module.css'
 
 const ScreenAddToCart = () => {
   return (
     <>
-    <div>
+    <div className={styles.titleCart}>
         App carrito de compra
     </div>
 
