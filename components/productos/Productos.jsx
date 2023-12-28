7183a962db31b4ed09885336385e4cff6e46fa4c
Paula Cano <paulacanoroman@gmail.com>
2023-12-28 11:23:16 +0100

    feat: style file Productos.module.css

--- components/productos/Productos.jsx
+++ components/productos/Productos.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import styles from '@/styles/Home.module.css'
+import styles from './Productos.module.css'
 import { CartContext } from '../../context/CartContextApp.jsx'
 
 const Productos = () => {
